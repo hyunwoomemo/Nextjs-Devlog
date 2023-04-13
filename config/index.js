268cffce4162b70f5d8442f639a723139de028fb
hyunwoomemo <archihw94@gmail.com>
2023-04-13 12:35:59 +0900

    update

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
-export const DATABASE_ID = process.env.NOTION_DB_ID
-export const TOKEN = process.env.NOTION_TOKEN
+export const DATABASE_ID = process.env.NEXT_PUBLIC_NOTION_DB_ID
+export const TOKEN = process.env.NEXT_PUBLIC_NOTION_TOKEN
 
 console.log(TOKEN, DATABASE_ID);
