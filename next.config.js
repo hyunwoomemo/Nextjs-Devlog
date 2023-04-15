10dd02639909d4f16728a9cacec88f04bda05ee1
hyunwoomemo <archihw94@gmail.com>
2023-04-15 22:07:38 +0900

    update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const withPlugins = require('next-compose-plugins');
+const withWorkbox = require('next-with-workbox');
 const withPWA = require('next-pwa');
 
 const nextConfig = {
@@ -19,15 +20,23 @@ const nextConfig = {
 
 module.exports = withPlugins(
   [
-    [
-      withPWA,
-      {
-        pwa: {
-          dest: "public",
-        },
+    withPWA,
+    {
+      pwa: {
+        dest: "public",
       },
-    ],
-    // 추가 플러그인 작성
+    },
   ],
+  // 추가 플러그인 작성
+  [
+    withWorkbox,
+    {
+      dest: 'public',
+      swDest: 'sw.js',
+      swSrc: 'worker.js',
+      force: true,
+    }
+  ]
+  ,
   nextConfig
 );
