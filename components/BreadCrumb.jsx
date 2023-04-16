4c13a72609ef3f80dd734b73f8f1704212fef39c
hyunwoomemo <archihw94@gmail.com>
2023-04-17 03:39:49 +0900

    update

--- components/BreadCrumb.jsx
+++ components/BreadCrumb.jsx
@@ -15,21 +15,45 @@ const BreadCrumb = ({ data }) => {
   };
 
   const filterData = data?.results.map((v) => v.properties.category?.select?.name).filter((v1, i, arr) => arr.indexOf(v1) === i);
-  console.log(filterData);
-
-  const [arr] = data?.results.map((v) => [v.id, v.properties.category?.select?.name, v.properties?.이름?.title[0]?.plain_text]).filter((v1, i, arr) => v1[0] === router.query.id);
-
-  console.log(arr);
 
   return (
     <Base style={{ display: router.pathname === "/" ? "none" : "block" }}>
       <BreadCrumbWrapper>
         <BreadCrumbItem href="/">home</BreadCrumbItem>
-        <BreadCrumbItem href={router.pathname.indexOf("blog") > -1 ? "/blog" : "/projects"}>{router.pathname.indexOf("blog") > -1 ? "blog" : "projects"}</BreadCrumbItem>
-        <BreadCrumbItem href="/blog/categories">
-          {router.query.id ? arr[1] : router.query.category ? router.query.category : router.pathname.lastIndexOf("categories") === 6 ? "카테고리 선택" : "전체 게시글"}
+        <BreadCrumbItem href={router.pathname.indexOf("blog") > -1 ? "/blog" : "/projects"}>
+          {router.pathname.indexOf("blog") > -1 ? (
+            <span>
+              ✏️<span>blog</span>
+            </span>
+          ) : (
+            "🚀projects"
+          )}
         </BreadCrumbItem>
-        {router.query.id ? <span>{arr[2].length > 5 ? arr[2].slice(0, arr[2].indexOf(" ")) + "..." : arr[2]}</span> : undefined}
+        <BreadCrumbItem href="/blog/categories">
+          {router.query.id
+            ? data?.results.map((v) => [v.id, v.properties.category?.select?.name, v.properties?.이름?.title[0]?.plain_text]).filter((v1, i, arr) => v1[0] === router.query.id)[0][1]
+            : router.query.category
+            ? router.query.category
+            : router.pathname.lastIndexOf("categories") === 6
+            ? "카테고리 선택"
+            : "전체 게시글"}
+        </BreadCrumbItem>
+        {/* {router.query.id ? (
+          <span>
+            {data?.results.map((v) => [v.id, v.properties.category?.select?.name, v.properties?.이름?.title[0]?.plain_text]).filter((v1, i, arr) => v1[0] === router.query.id)[0][2].length > 5
+              ? data?.results
+                  .map((v) => [v.id, v.properties.category?.select?.name, v.properties?.이름?.title[0]?.plain_text])
+                  .filter((v1, i, arr) => v1[0] === router.query.id)[0][2]
+                  .slice(
+                    0,
+                    data?.results
+                      .map((v) => [v.id, v.properties.category?.select?.name, v.properties?.이름?.title[0]?.plain_text])
+                      .filter((v1, i, arr) => v1[0] === router.query.id)[0][2]
+                      .indexOf(" ")
+                  ) + "..."
+              : data?.results.map((v) => [v.id, v.properties.category?.select?.name, v.properties?.이름?.title[0]?.plain_text]).filter((v1, i, arr) => v1[0] === router.query.id)[0][2]}
+          </span>
+        ) : undefined} */}
         <Modal isOpen={isOpen} onClose={handleClose} position="center">
           <ModalBody>
             {filterData?.map((d, i) => {
@@ -55,15 +79,37 @@ const BreadCrumbWrapper = styled.ul`
 
   > span {
     &:before {
-      content: "/";
-      margin-right: 5px;
+      content: "📄";
+      font-size: 12px;
     }
   }
 `;
 
 const BreadCrumbItem = styled(Link)`
+  &:first-of-type:before {
+    content: "🏠";
+    font-size: 12px;
+  }
+
+  &:nth-of-type(2) > span {
+    font-size: 12px;
+
+    > span {
+      font-size: 16px;
+    }
+  }
+
+  &:nth-of-type(3) {
+    color: #346996;
+    &:before {
+      content: "📚";
+      font-size: 12px;
+    }
+  }
+
   &:after {
-    content: "/";
+    content: "▪️";
+    color: var(--text-color);
     margin-left: 5px;
   }
 
