02d7e2b963e4755a2b7cec455e1ff1c0e5d26e98
hyunwoomemo <archihw94@gmail.com>
2023-04-23 06:32:28 +0900

    update

---
+++ components/RollingBanner.jsx
@@ -0,0 +1,78 @@
+import styled from "@emotion/styled";
+import React, { useEffect, useState } from "react";
+
+const RollingBanner = ({ speed, children }) => {
+  const [moveX, setMoveX] = useState(0);
+  const [direction, setDirection] = useState(1);
+
+  useEffect(() => {
+    if (typeof window !== "object") return;
+
+    if (children.length > 3) {
+      const scroll = document.querySelector("#slide-wrapper").scrollWidth;
+      const offset = document.querySelector("#slide-wrapper").offsetWidth;
+
+      const check = Math.floor(scroll / offset);
+
+      const move = setInterval(() => {
+        setMoveX((prev) => prev + speed * direction);
+        document.querySelector("#slide-wrapper").style.transform = `translateX(-${moveX}px)`;
+      }, 100);
+
+      if (moveX >= offset * (check - 1) + scroll - offset * check) {
+        setDirection(-1);
+      } else if (moveX <= 0) {
+        setDirection(1);
+      }
+
+      return () => {
+        clearInterval(move);
+      };
+    }
+  }, [moveX, direction]);
+
+  return (
+    <Container id="container">
+      <SlideWrapper id="slide-wrapper">
+        {children.length > 1 ? (
+          children.map((v, i) => {
+            return <SlideItem key={i}>{v}</SlideItem>;
+          })
+        ) : (
+          <SlideItem>{children}</SlideItem>
+        )}
+      </SlideWrapper>
+    </Container>
+  );
+};
+
+export default RollingBanner;
+
+const Container = styled.div`
+  overflow: hidden;
+`;
+
+const SlideWrapper = styled.ul`
+  display: flex;
+  gap: 1rem;
+  padding: 2rem;
+  @media (max-width: 768px) {
+    padding: 1rem;
+  }
+  transition: all 0.3s;
+`;
+
+const SlideItem = styled.li`
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-width: 150px;
+
+  > img {
+    width: 100%;
+    max-width: 75px;
+    height: auto;
+  }
+`;
--- pages/index.js
+++ pages/index.js
@@ -6,20 +6,36 @@ import Layout from '@/components/Layout'
 import Hero from '@/components/Hero'
 import styled from '@emotion/styled'
 import Footer from '@/components/Footer'
-
-const inter = Inter({ subsets: ['latin'] })
+import { useEffect, useState } from 'react'
+import RollingBanner from '@/components/RollingBanner'
+import github from '@/public/languageIcons/github.png';
+import react from '@/public/languageIcons/react.png';
+import javascript from '@/public/languageIcons/javascript.png';
+import html from '@/public/languageIcons/html.png';
+import nodejs from '@/public/languageIcons/nodejs.png';
+import express from '@/public/languageIcons/express.png';
+import mysql from '@/public/languageIcons/mysql.png';
 
 export default function Home() {
+
   return (
     <Base>
       <Layout>
         <Seo title="home" />
         <Hero />
+        <RollingBanner speed={5}>
+          <Image alt="language-icons" src={github}></Image>
+          <Image alt="language-icons" src={react}></Image>
+          <Image alt="language-icons" src={javascript}></Image>
+          <Image alt="language-icons" src={html}></Image>
+          <Image alt="language-icons" src={nodejs}></Image>
+          <Image alt="language-icons" src={express}></Image>
+          <Image alt="language-icons" src={mysql}></Image>
+        </RollingBanner>
       </Layout>
     </Base>
   )
 }
 
 const Base = styled.div`
-
 `
