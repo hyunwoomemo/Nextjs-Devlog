27b7e5c72cf605be968b118e55c23bff04bb94b1
hyunwoomemo <archihw94@gmail.com>
2023-04-27 23:41:33 +0900

    update

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,16 +1,36 @@
 import Layout from "@/components/Layout";
 import styled from "@emotion/styled";
+import { NextSeo } from "next-seo";
 import Link from "next/link";
 import React from "react";
 
 const index = () => {
   return (
-    <Layout>
-      <Wrapper>
-        <MenuItem href="/blog/posts">Posts</MenuItem>
-        <MenuItem href="blog/codesnipet">Code Snipet</MenuItem>
-      </Wrapper>
-    </Layout>
+    <>
+      <NextSeo
+        title="Blog | Hyunwoomemo"
+        description="프론트엔드 개발자의 기술 블로그, 다양한 주제의 글로 새로운 지식을 기록합니다."
+        openGraph={{
+          type: "website",
+          url: "https://hyunwoomemo.vercel.app/",
+          title: "Hyunwoomemo's Devlog",
+          description: "프론트엔드 개발자의 기술 블로그, 다양한 주제의 글로 새로운 지식을 기록합니다.",
+          images: [
+            {
+              url: "https://user-images.githubusercontent.com/105469077/234896480-32d59948-f5fb-4232-823b-38bb12bb34d6.png",
+              width: 800,
+              height: 400,
+            },
+          ],
+        }}
+      />
+      <Layout>
+        <Wrapper>
+          <MenuItem href="/blog/posts">Posts</MenuItem>
+          <MenuItem href="blog/codesnipet">Code Snipet</MenuItem>
+        </Wrapper>
+      </Layout>
+    </>
   );
 };
 
--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -2,13 +2,33 @@ import Layout from "@/components/Layout";
 import ProjectList from "@/components/projects/ProjectList";
 import { TOKEN, PROJECT_DATABASE_ID, POST_DATABASE_ID } from "@/config";
 import styled from "@emotion/styled";
+import { NextSeo } from "next-seo";
 
 const index = ({ projects }) => {
   return (
-    <Layout headerTitle="Projects">
-      <Title>진행한 프로젝트</Title>
-      <ProjectList data={projects} />
-    </Layout>
+    <>
+      <NextSeo
+        title="Project | Hyunwoomemo"
+        description="프론트엔드 개발자의 기술 블로그, 다양한 주제의 글로 새로운 지식을 기록합니다."
+        openGraph={{
+          type: "website",
+          url: "https://hyunwoomemo.vercel.app/",
+          title: "Hyunwoomemo's Devlog",
+          description: "프론트엔드 개발자의 기술 블로그, 다양한 주제의 글로 새로운 지식을 기록합니다.",
+          images: [
+            {
+              url: "https://user-images.githubusercontent.com/105469077/234896480-32d59948-f5fb-4232-823b-38bb12bb34d6.png",
+              width: 800,
+              height: 400,
+            },
+          ],
+        }}
+      />
+      <Layout headerTitle="Projects">
+        <Title>진행한 프로젝트</Title>
+        <ProjectList data={projects} />
+      </Layout>
+    </>
   );
 };
 
