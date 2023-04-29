42ba34978eef69f1d9414255747d4ca8544b1d6f
hyunwoomemo <archihw94@gmail.com>
2023-04-29 22:33:29 +0900

    update

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,37 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { CacheProvider } from '@emotion/react'
+import { renderToString } from 'react-dom/server'
+import createEmotionServer from '@emotion/server/create-instance'
+import createCache from '@emotion/cache'
+import App from '../_app'
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+const key = 'custom'
+const cache = createCache({ key })
+const { extractCriticalToChunks, constructStyleTagsFromChunks } = createEmotionServer(cache)
+
+const html = renderToString(
+  <CacheProvider value={cache}>
+    <App />
+  </CacheProvider>
+)
+
+const chunks = extractCriticalToChunks(html)
+const styles = constructStyleTagsFromChunks(chunks)
+
+res
+  .status(200)
+  .header('Content-Type', 'text/html')
+  .send(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My site</title>
+    ${styles}
+</head>
+<body>
+    <div id="root">${html}</div>
+
+    <script src="./bundle.js"></script>
+</body>
+</html>`);
