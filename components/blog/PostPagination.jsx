e1b64ca18bf54cbe4467779b7eef1ff959fbc10f
hyunwoomemo <archihw94@gmail.com>
2023-05-01 19:51:11 +0900

    update

--- components/blog/PostPagination.jsx
+++ components/blog/PostPagination.jsx
@@ -9,8 +9,8 @@ import { useSelector } from "react-redux";
 
 export default function PostPagination({ numPages }) {
   const router = useRouter();
-  console.log(numPages);
 
+  const groupCount = 4;
   const [page, setPage] = useState(1);
 
   const { selectedCategory, selectedTag } = useSelector((state) => state.FilterSlice);
@@ -27,6 +27,11 @@ export default function PostPagination({ numPages }) {
     if (selectedTag) {
       query.tag = selectedTag;
     }
+
+    if ((page - 1) % 4 === 0 && page !== 1) {
+      query.pageGroup = Math.floor((page - 2) / 4) + 1;
+    }
+
     router.push({ pathname: "/blog/posts", query });
     setPage((prev) => prev - 1);
   };
@@ -39,6 +44,11 @@ export default function PostPagination({ numPages }) {
     if (selectedTag) {
       query.tag = selectedTag;
     }
+
+    if ((page + 1) % groupCount === 1 && page !== 1) {
+      query.pageGroup = Math.floor((page - 1) / groupCount) * groupCount + 5;
+    }
+
     router.push({ pathname: "/blog/posts", query });
     setPage((prev) => prev + 1);
   };
@@ -56,6 +66,22 @@ export default function PostPagination({ numPages }) {
     setPage(parseInt(e.target.innerText));
   };
 
+  const renderPaginationItems = () => {
+    const start = Math.floor((page - 1) / groupCount) * groupCount;
+    const end = Math.min(numPages, start + groupCount);
+    const items = [];
+
+    for (let i = start; i < end; i++) {
+      items.push(
+        <PaginationItem page={page === i + 1} key={i} onClick={(e) => handleNumberClick(e)}>
+          {i + 1}
+        </PaginationItem>
+      );
+    }
+
+    return items;
+  };
+
   return (
     <Base>
       <Pagination>
@@ -64,20 +90,7 @@ export default function PostPagination({ numPages }) {
             <path fill-rule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clip-rule="evenodd" />
           </svg>
         </PrevBtn>
-        {new Array(numPages)
-          .fill(0)
-          .slice(0, 3)
-          .map((v, i) => {
-            return (
-              <PaginationItem page={page === i + 1} key={i} onClick={(e) => handleNumberClick(e)}>
-                {i + 1}
-              </PaginationItem>
-            );
-          })}
-        ...
-        <PaginationItem page={page === numPages} key={numPages} onClick={(e) => handleNumberClick(e)}>
-          {numPages}
-        </PaginationItem>
+        {renderPaginationItems()}
         <NextBtn disabled={page === numPages} onClick={handleNextClick}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
             <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
