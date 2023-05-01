e20a7cdeb43a9a5b81fc13d35757c6f57a4720fd
hyunwoomemo <archihw94@gmail.com>
2023-05-02 03:35:42 +0900

    update

--- components/about/AboutProjectList.jsx
+++ components/about/AboutProjectList.jsx
@@ -1,4 +1,5 @@
 import { darkThemeTagColor, lightThemeTagColor } from "@/util/backgroundColor";
+import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 import dayjs from "dayjs";
 import Image from "next/image";
@@ -6,54 +7,67 @@ import Link from "next/link";
 import React from "react";
 import { useSelector } from "react-redux";
 
+// 프로젝트 날짜
+const date = (start, end) => {
+  const startDate = dayjs(new Date(start));
+  const endDate = dayjs(new Date(end));
+
+  if (startDate.format("YYYY-MM") === endDate.format("YYYY-MM")) {
+    return `${startDate.format("YYYY-MM-DD")} ~ ${endDate.format("YYYY-MM-DD")}`;
+  } else {
+    return `${startDate.format("YYYY-MM")} ~ ${endDate.format("YYYY-MM")}`;
+  }
+};
+
+// 프로젝트 내용 렌더링하는 함수
+const getContents = (data) => {
+  return (
+    <ContentWrapper>
+      {data.map((v) => {
+        const content = v.name;
+        return <ContentItem key={content}>{content}</ContentItem>;
+      })}
+    </ContentWrapper>
+  );
+};
+
+// 프로젝트 키워드 렌더링하는 함수
+const getKeyword = (data) => {
+  return (
+    <KeywordWrapper>
+      {data.map((v) => {
+        const keyword = v.name;
+        return <KeywordItem key={keyword}>{keyword}</KeywordItem>;
+      })}
+    </KeywordWrapper>
+  );
+};
+
 const AboutProjectList = ({ data }) => {
   const filterData = data.results.filter((v) => v.properties.상태.status.name === "Done");
 
-  const calculatedPeriod = (start, end) => {
-    const startDateStringArray = start.split("-");
-    const endDateStringArray = end.split("-");
-
-    var startDate = new Date(startDateStringArray[0], startDateStringArray[1], startDateStringArray[2]);
-    var endDate = new Date(endDateStringArray[0], endDateStringArray[1], endDateStringArray[2]);
-
-    const diffInMs = Math.abs(endDate - startDate);
-    const result = diffInMs / (1000 * 60 * 60 * 24);
-
-    return result;
-  };
+  console.log(filterData);
 
   return (
     <Base>
       {filterData?.map((project) => {
+        const imgSrc = project?.cover?.file?.url || project?.cover?.external?.url;
         const title = project.properties.Name.title[0]?.plain_text;
         const description = project.properties.summary.rich_text[0]?.plain_text;
-        const imgSrc = project?.cover?.file?.url || project?.cover?.external?.url;
-        const tags = project.properties.tags?.multi_select;
         const id = project.id;
         const start = project.properties.날짜?.date?.start;
         const end = project.properties.날짜?.date?.end;
-        const date = (start, end) => {
-          const startDate = dayjs(new Date(start));
-          const endDate = dayjs(new Date(end));
-
-          if (startDate.format("YYYY-MM") === endDate.format("YYYY-MM")) {
-            return `${startDate.format("YYYY-MM-DD")} ~ ${endDate.format("YYYY-MM-DD")}`;
-          } else {
-            return `${startDate.format("YYYY-MM")} ~ ${endDate.format("YYYY-MM")}`;
-          }
-        };
+        const contents = project.properties.내용.multi_select;
+        const keywords = project.properties.keyword.multi_select;
 
         return (
-          <Project href={`/projects/${id}`} key={project.id}>
+          <Project href={`/projects/${id}`} key={project.id} imgSrc={imgSrc}>
             {start && end ? <ProjectDate>{date(start, end)}</ProjectDate> : undefined}
             <Wrapper>
               <Title>{title}</Title>
               <Desc>{description}</Desc>
-              <Tags>
-                {tags.map((tag) => {
-                  return <li key={tag.id}>{tag.name}</li>;
-                })}
-              </Tags>
+              {getContents(contents)}
+              {getKeyword(keywords)}
             </Wrapper>
           </Project>
         );
@@ -82,28 +96,25 @@ const Project = styled(Link)`
   transition: all 0.3s;
   flex-wrap: wrap;
   padding: 1rem 0;
+
+  ${({ imgSrc }) =>
+    imgSrc
+      ? css`
+          background-image: url(${imgSrc});
+          background-position: 100% 16px;
+          background-size: 70px;
+          background-repeat: no-repeat;
+        `
+      : css``}
 `;
 
 const ProjectDate = styled.div`
   word-wrap: normal;
   width: 100%;
-`;
-
-const ImageItem = styled(Image)`
-  border-radius: 5px;
-  object-fit: cover;
-  width: 100%;
-  width: 100px;
-  height: 100px;
-
-  @media (max-width: 768px) {
-    width: 75px;
-    height: 75px;
-  }
+  color: gray;
 `;
 
 const Wrapper = styled.div`
-  /* padding: 2rem 0; */
   display: flex;
   flex-direction: column;
   align-self: flex-start;
@@ -114,38 +125,51 @@ const Title = styled.h1`
   font-size: 20px;
   font-weight: bold;
   @media (max-width: 768px) {
-    font-size: 12px;
+    font-size: 18px;
     line-height: 14px;
   }
 `;
 
+const ContentWrapper = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding-top: 1rem;
+`;
+
+const ContentItem = styled.li`
+  list-style: disc;
+  font-size: 12px;
+  line-height: 16px;
+`;
+
 const Desc = styled.h2`
   color: gray;
+  font-style: italic;
   @media (max-width: 768px) {
-    font-size: 10px;
+    font-size: 12px;
     line-height: 12px;
   }
 `;
 
 const Period = styled.span``;
 
-const Tags = styled.ul`
+const KeywordWrapper = styled.ul`
   display: flex;
   gap: 10px;
   flex-wrap: wrap;
   margin-top: auto;
+`;
 
-  li {
-    font-size: 12px;
-    padding: 5px;
-    border-radius: 5px;
-    color: var(--text-color);
-    white-space: nowrap;
+const KeywordItem = styled.li`
+  font-size: 14px;
+  padding: 4px 6px;
+  border-radius: 5px;
+  color: #ffffff;
+  white-space: nowrap;
+  background-color: gray;
 
-    @media (max-width: 768px) {
-      font-size: 10px;
-      padding: 3px;
-    }
+  @media (max-width: 768px) {
   }
 `;
 
