c42c63713cde37a191d19d5dc6d0fe20440c2edd
hyunwoomemo <archihw94@gmail.com>
2023-05-05 07:29:50 +0900

    페이지의 일정 부분 넘어가면 위로 올라가는 top 버튼 구현

--- pages/blog/posts/[id].jsx
+++ pages/blog/posts/[id].jsx
@@ -18,6 +18,8 @@ import Toc from "@/components/blog/Toc";
 import remarkGfm from "remark-gfm";
 import { NextSeo } from "next-seo";
 import { useRouter } from "next/router";
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
 
 const PostItem = ({ html_text, posts, toc }) => {
   const router = useRouter();
@@ -25,6 +27,26 @@ const PostItem = ({ html_text, posts, toc }) => {
   const title = filterPosts[0].properties.Name.title[0].plain_text;
   const img = filterPosts[0].cover?.file?.url || filterPosts[0].cover?.external.url;
   console.log(img);
+  const [scrollTop, setScrollTop] = useState(0);
+  const [offset, setOffset] = useState(0);
+
+  const handleScroll = () => {
+    setScrollTop(document.documentElement.scrollTop);
+  };
+
+  useEffect(() => {
+    if (typeof window !== "object") return;
+
+    window.addEventListener("scroll", handleScroll);
+    setOffset(document.documentElement.scrollHeight);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
+  console.log(scrollTop, offset);
+
   return (
     <>
       <NextSeo
@@ -48,10 +70,45 @@ const PostItem = ({ html_text, posts, toc }) => {
         <PostHeader data={posts}></PostHeader>
         {toc.json.length > 0 ? <Toc toc={toc}></Toc> : undefined}
         <Markdown2Html html={html_text} />
+        <TopBtn active={scrollTop > offset * 0.3} onClick={() => scrollTo({ top: 0, behavior: "smooth" })}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+          </svg>
+        </TopBtn>
       </Layout>
     </>
   );
 };
+
+const TopBtn = styled.div`
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  bottom: 15px;
+  right: 15px;
+  width: 40px;
+  border-radius: 50%;
+  padding: 10px;
+  background-color: var(--text-color);
+  color: var(--main-background);
+  transition: all 0.3;
+  svg {
+    stroke: var(--main-background);
+  }
+
+  ${({ active }) =>
+    active
+      ? css`
+          opacity: 1;
+          pointer-events: all;
+        `
+      : css`
+          opacity: 0;
+          pointer-events: none;
+        `}
+`;
 
 export default PostItem;
 export async function getStaticPaths() {
